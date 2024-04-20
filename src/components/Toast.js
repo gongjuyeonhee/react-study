a4994a409397a94be0fe2a75619399de3965f383
gongjuyeonhee <yyeonhee912@naver.com>
2024-04-20 15:54:15 +0900

    Toast 컴포넌트 분리 리팩토링

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./Toast.css";
+import ToastMessage from "./ToastMessage";
+import ToastButton from "./ToastButton";
 
 const Toast = ({ message }) => {
   return (
     <div className={`toast toast-${message.title}`}>
-      <main className="toast__message">
-        <header className="toast__message-title">{message.title}</header>
-        <p className="toast__message-text">{message.text}</p>
-      </main>
-      <button className="toast__button">Dismiss</button>
+      <ToastMessage message={message} />
+      <ToastButton />
     </div>
   );
 };
