8d4e5f1ab3908f7cd261dbc7a63f6fe3c30c2b7e
gongjuyeonhee <yyeonhee912@naver.com>
2024-04-23 12:15:32 +0900

    form-input 실습

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,24 +1,12 @@
-import React, { useState } from "react";
-import "./Toast.css";
-import ToastMessage from "./ToastMessage";
+import React from 'react';
+import './Toast.css';
+import ToastMessage from './ToastMessage';
 
 const Toast = ({ message }) => {
-  const [text, setText] = useState("sample");
-  const buttonClickHandler = (title) => {
-    setText("changed!");
-    console.log(title);
-  };
-
   return (
     <div className={`toast toast-${message.title}`}>
-      <ToastMessage message={message} />
-      <p>{text}</p>
-      <button
-        className="toast__button"
-        onClick={() => buttonClickHandler(message.title)}
-      >
-        Dismiss
-      </button>
+      <ToastMessage message={message}/>
+      <button className="toast__button">Dismiss</button>
     </div>
   );
 };
