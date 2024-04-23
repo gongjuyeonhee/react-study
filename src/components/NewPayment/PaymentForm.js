b46b9d6512009fa8741a6cf81c712a46052babaf
gongjuyeonhee <yyeonhee912@naver.com>
2024-04-23 14:00:47 +0900

    각각 개별의 useState만들고 제출 버튼 클릭 시 콘솔출력

--- src/components/NewPayment/PaymentForm.js
+++ src/components/NewPayment/PaymentForm.js
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./PaymentForm.css";
 
 const PaymentForm = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [today, setToday] = useState(null);
+
+  const textChangeHandler = (event) => {
+    setName(event.target.value);
+  };
+  const numberChangeHandler = (event) => {
+    setPrice(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setToday(event.target.value);
+  };
+  const buttonSubmitHandler = () => {
+    console.log(name, price, today);
+  };
   return (
     <form>
       <div className="new-payment__controls">
         <div className="new-payment__control">
           <label>이름</label>
-          <input type="text" />
+          <input type="text" value={name} onChange={textChangeHandler} />
         </div>
         <div className="new-payment__control">
           <label>금액</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={price}
+            onChange={numberChangeHandler}
+          />
         </div>
         <div className="new-payment__control">
           <label>날짜</label>
-          <input type="date" min="2024-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2024-12-31"
+            value={today}
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-payment__actions">
-        <button type="submit">결제 추가</button>
+        <button type="button" onClick={buttonSubmitHandler}>
+          결제 추가
+        </button>
       </div>
     </form>
   );
