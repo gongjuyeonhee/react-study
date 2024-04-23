ae5fb4cc73d9d67f58127556dd17f548b823b1b1
gongjuyeonhee <yyeonhee912@naver.com>
2024-04-23 18:14:49 +0900

    자식 컴포넌트에 부모 컴포넌트 상태 내려주기 & ExpenseDate 안의 날짜 문자열 형태에서 Date 객체로 변환

--- src/components/Payments/ExpenseDate.js
+++ src/components/Payments/ExpenseDate.js
@@ -3,9 +3,10 @@ import React from "react";
 import "./ExpenseDate.css";
 
 const ExpenseDate = (props) => {
-  const month = props.date.toLocaleString("ko-KR", { month: "long" });
-  const day = props.date.toLocaleString("ko-KR", { day: "2-digit" });
-  const year = props.date.getFullYear();
+  const dateObj = new Date(props.date); // 문자열을 Date 객체로 변환
+  const month = dateObj.toLocaleString("ko-KR", { month: "long" });
+  const day = dateObj.toLocaleString("ko-KR", { day: "2-digit" });
+  const year = dateObj instanceof Date ? dateObj.getFullYear() : null;
 
   return (
     <div className="expense-date">
