c358ef956ba6b7365b43fe44ab7f089038eac1a5
gongjuyeonhee <yyeonhee912@naver.com>
2024-04-23 16:28:54 +0900

    부모 컴포넌트에 자식 컴포넌트 상태를 올려주기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
-import NewPayment from './components/NewPayment/NewPayment';
+import { useState } from "react";
+import "./App.css";
+import PaymentForm from "./components/PaymentForm/PaymentForm";
+import Expenses from "./components/Payments/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "수건",
+      amount: 12.33,
+      date: new Date(2025, 8, 14),
+    },
+  ];
+
+  const getPaymentFormData = (data) => {
+    console.log(data);
+  };
 
   return (
-    <NewPayment/>
+    <>
+      <PaymentForm getPaymentFormData={getPaymentFormData} />
+      <Expenses items={expenses} />
+    </>
   );
 }
 
