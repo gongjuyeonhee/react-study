fb610a27ae248845a82dbaa26cb53736c53e9a6b
gongjuyeonhee <yyeonhee912@naver.com>
2024-04-24 15:47:55 +0900

    인라인 스타일 적용 &  tirm메소드를 사용해 글자 공백 삭제 적용

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import "./App.css";
-import Toast from "./components/Toast";
+import React, { useState } from 'react';
 
-function App() {
-  const messageArray = [
-    {
-      title: "success",
-      text: "Right On! Your account has been updated.",
-    },
-    {
-      title: "warning",
-      text: "Hmmm. Something doesn't look right.",
-    },
-    {
-      title: "error",
-      text: "Un oh! Something went terribly wrong!",
-    },
-  ];
+import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
+import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
+import './App.css';
+
+const App = () => {
+  const [courseGoals, setCourseGoals] = useState([
+    { text: '운동하기', id: 'g1' },
+    { text: '완강하기', id: 'g2' }
+  ]);
+
+  const addGoalHandler = enteredText => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
+  const deleteItemHandler = goalId => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+
+  let content = (
+    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+  );
+
+  if (courseGoals.length > 0) {
+    content = (
+      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+    );
+  }
 
   return (
-    <div className="wrapper">
-      {messageArray.map((message) => (
-        <Toast message={message} />
-      ))}
+    <div>
+      <section id="goal-form">
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+      <section id="goals">
+        {content}
+        {/* {courseGoals.length > 0 && (
+          <CourseGoalList
+            items={courseGoals}
+            onDeleteItem={deleteItemHandler}
+          />
+        ) // <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+        } */}
+      </section>
     </div>
   );
-}
+};
+
 export default App;
---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      //입력 시 앞뒤로 공백이 있지만 글자가 포함된 경우
+      setIsValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    //값이 '' 경우, 배경을 빨간색으로 해 주어야 한다.
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      setEnteredValue("");
+      return; //여기서 리턴 꼭 해주기. (안그러면 밑줄 실행됨)
+    }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label>목표</label>
+        <input
+          type="text"
+          onChange={goalInputChangeHandler}
+          style={{
+            backgroundColor: isValid ? "transparent" : "salmon",
+            borderColor: isValid ? "#ccc" : "red",
+          }}
+        />
+      </div>
+      <Button type="submit">목표 추가하기</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
