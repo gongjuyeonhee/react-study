97eb91f3d19a729b808997d4502a76e0f42bfca4
gongjuyeonhee <yyeonhee912@naver.com>
2024-04-24 15:05:44 +0900

    리스트 필터링: JSX문법 안에 삼항 연산자 사용해서 넣어주기

--- src/components/Payments/Expenses.js
+++ src/components/Payments/Expenses.js
@@ -16,18 +16,18 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expenseContent = <p>값이 없습니다</p>;
+  // let expenseContent = <p>값이 없습니다</p>;
 
-  if (filteredExpenses.length > 0) {
-    expenseContent = filteredExpenses.map((item) => (
-      <ExpenseItem
-        key={item.id}
-        title={item.title}
-        amount={item.amount}
-        date={item.date}
-      />
-    ));
-  }
+  // if (filteredExpenses.length > 0) {
+  //   expenseContent = filteredExpenses.map((item) => (
+  //     <ExpenseItem
+  //       key={item.id}
+  //       title={item.title}
+  //       amount={item.amount}
+  //       date={item.date}
+  //     />
+  //   ));
+  // }
 
   return (
     <Card className="expenses">
@@ -35,7 +35,18 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {expenseContent}
+      {filteredExpenses.length > 0 ? (
+        filteredExpenses.map((item) => (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        ))
+      ) : (
+        <p>값이 없습니다</p>
+      )}
     </Card>
   );
 };
