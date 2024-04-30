94ab19c26b836efc57fecd255ebd3cdd8202af61
gongjuyeonhee <yyeonhee912@naver.com>
2024-04-30 17:21:58 +0900

    localStorage를 사용: 새로고침 이후에도 로그인 유지 가능

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
+
+    //만약 로그인이 되어 있다면 isLoggedIn = true
+    if (storedUserLoggedInInfo === "1") setIsLoggedIn(true);
+    //만약 로그인이 되어 있지 않다면 isLoggedIn = false
+    else setIsLoggedIn(false);
+  }, []);
+
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1"); //key, value
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
