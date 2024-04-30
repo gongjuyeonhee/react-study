bf57d93f4bcc70fc4c3a2ffa19c3fd3f369fe923
gongjuyeonhee <yyeonhee912@naver.com>
2024-04-30 15:14:50 +0900

    로그인 화면: useEffect 적용 전 코드

--- src/App.js
+++ src/App.js
@@ -1,59 +1,31 @@
-import { useState } from "react";
-import "./App.css";
-import PaymentForm from "./components/PaymentForm/PaymentForm";
-import Expenses from "./components/Payments/Expenses";
+import React, { useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
-	const [expenses, setExpenses] = useState([
-		{
-			id: "e1",
-			title: "수건",
-			amount: 12.33,
-			date: new Date(2022, 3, 14),
-		},
-		{
-			id: "e2",
-			title: "물티슈",
-			amount: 234.11,
-			date: new Date(2023, 8, 22),
-		},
-		{
-			id: "e3",
-			title: "모니터",
-			amount: 3331.33,
-			date: new Date(2021, 10, 22),
-		},
-		{
-			id: "e4",
-			title: "의자",
-			amount: 298,
-			date: new Date(2022, 1, 31),
-		},
-		{
-			id: "e4",
-			title: "의자",
-			amount: 298,
-			date: new Date(2021, 6, 1),
-		},
-	]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-	const getPaymentFormData = (data) => {
-		setExpenses([
-			{
-				id: Math.random().toString(),
-				title: data.name,
-				amount: data.price,
-				date: new Date(data.today),
-			},
-		]);
-	};
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
 
-	return (
-		<>
-			<PaymentForm getPaymentFormData={getPaymentFormData} />
-			<Expenses items={expenses} />
-		</>
-	);
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
